13cd48c78fdbc4caeb92650962a1e794c9685b81
NeerajAuti <nauti@uncc.edu>
2019-10-27 00:29:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hw06_group22/MyProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw06_group22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
